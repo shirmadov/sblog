354ba2e6fcb4975af073b8a9bd8e068c4ce9ae1e
Sapa <sapashirmadov2501@gmail.com>
2022-02-22 20:11:18 +0300

    New Editor

--- public/js/user/editor.js
+++ public/js/user/editor.js
@@ -1,60 +1,59 @@
 let app_url = location.origin;
 // let editor__text = document.querySelectorAll('.js__editor__text');
-
+// let count = 0;
 function keyDown() {
+    let count = 0;
     let editor__text = document.querySelectorAll('.js__editor__text');
 
     editor__text.forEach(function (item, index) {
         if (!item) return;
-        let count = 0;
-        console.log(item)
         item.addEventListener('keydown', (event) => {
 
-            if (event.keyCode == 13 && count == 0) {
-                count++
-                console.log('Count:',count)
+            // event.preventDefault();
+            if ( event.keyCode == 13 && count==0) {
+                count++;
+
                 let editor_main = item.parentElement;
-                createLine();
+                let plus_icon = '   <div class="editor__plus__main">\n' +
+                    '                       <svg class="editor__plus" style="margin-right: 30px;" xmlns="http://www.w3.org/2000/svg" width="35" height="24" viewBox="0 0 40 32" fill="none">\n' +
+                    '                           <rect x="0.5" y="0.75" width="39" height="30.5" rx="6.5" fill="#FFFBFB" stroke="black"/>\n' +
+                    '                           <line x1="10" y1="15.5" x2="30" y2="15.5" stroke="black"/>\n' +
+                    '                           <line x1="20.1667" y1="23.752" x2="20.1667" y2="7" stroke="black"/>\n' +
+                    '                       </svg>\n' +
+                    '                   </div>'
+
+                let create_div = document.createElement('div');
+                let create_input = document.createElement('input');
+
+                create_div.classList.add('editor__line');
+                create_div.classList.add('js__editor__line');
+                create_input.placeholder = "New One";
+                create_input.type = 'text';
+                create_input.style.marginLeft = '4px';
+                create_input.classList.add('editor__text');
+                create_input.classList.add('js__editor__text');
+
+                create_div.innerHTML = plus_icon;
+                create_div.append(create_input);
+
+                editor_main.insertAdjacentElement('afterend', create_div);
+                create_input.focus();
+
+                keyDown()
 
 
-
-                // let plus_icon = '   <div class="editor__plus__main">\n' +
-                //     '                       <svg class="editor__plus" style="margin-right: 18px;" xmlns="http://www.w3.org/2000/svg" width="35" height="24" viewBox="0 0 40 32" fill="none">\n' +
-                //     '                           <rect x="0.5" y="0.75" width="39" height="30.5" rx="6.5" fill="#FFFBFB" stroke="black"/>\n' +
-                //     '                           <line x1="10" y1="15.5" x2="30" y2="15.5" stroke="black"/>\n' +
-                //     '                           <line x1="20.1667" y1="23.752" x2="20.1667" y2="7" stroke="black"/>\n' +
-                //     '                       </svg>\n' +
-                //     '                   </div>'
-                //
-                // let create_div = document.createElement('div');
-                // let create_input = document.createElement('input');
-                //
-                // create_div.classList.add('editor__line');
-                // create_div.classList.add('js__editor__line');
-                // create_input.placeholder = "New One";
-                // create_input.type = 'text';
-                // create_input.classList.add('editor__text');
-                // create_input.classList.add('js__editor__text');
-                //
-                // create_div.innerHTML = plus_icon;
-                // create_div.append(create_input);
-                //
-                // editor_main.insertAdjacentElement('afterend', create_div);
-                // create_input.focus();
-                //
-                // editor__text = document.querySelectorAll('.js__editor__text');
-                // console.log(editor__text)
-                // keyDown()
             } else if (event.keyCode == 40 && index + 1 < editor__text.length) {
                 editor__text[index + 1].focus();
             } else if (event.keyCode == 38 && index - 1 > 0) {
                 editor__text[index - 1].focus();
             } else if (event.keyCode == 8) {
                 let value = editor__text[index].value;
-                if (value.length == 0) {
+                if (value.length == 0 && index > 0) {
                     let editor_main = item.parentElement;
                     editor_main.remove();
-                    // editor_main.parentNode.removeChild(editor_main);
+                    if(index > 0){
+                        editor__text[index-1].focus();
+                    }
                 }
             }
         })
@@ -65,8 +64,8 @@ function keyDown() {
 async function createLine() {
     let url = app_url + '/editor/create_line';
     let formData = new FormData();
-    let editor__text = document.querySelectorAll('.js__editor__text');
-    formData.append('num_line', editor__text.length);
+    // let editor__text = document.querySelectorAll('.js__editor__text');
+    formData.append('num_line', 1);
 
     const response = await sendData(formData, url);
 
@@ -74,10 +73,8 @@ async function createLine() {
         let form = document.querySelector('.editorForm');
         console.log('Create New One');
         form.insertAdjacentHTML('beforeend', response.content);
-        keyDown();
+        // keyDown();
     }
-
-
 
 }
 
@@ -104,24 +101,6 @@ async function sendData(formData, url) {
 }
 
 
-function showPlus() {
-    let editor__text = document.querySelectorAll('.js__editor__text');
-    editor__text.forEach(function (item, index) {
-        if (!item) return;
-        item.addEventListener('click', (event) => {
-            console.log('Clicked Yes')
-
-        })
-    });
-}
-
-
-function push(list, items) {
-    Array.prototype.push.apply(list, items);
-    list.length_ = list.length;
-    list.length_ += items.length;
-}
-
 function removeLine(){
     document.addEventListener('keydown', (e)=>{
         const target = e.target;
@@ -135,16 +114,48 @@ function removeLine(){
                 createLine()
             }
         })
+    })
+}
+
+function keyDown2(){
+    addEventListener('keydown', (event)=>{
+
+        // e.key == 'Enter'
 
 
+        let editor__text = document.querySelectorAll('.js__editor__text');
+        console.log(editor__text)
+        editor__text.forEach((item, index)=>{
+            if (!item) return;
+            console.log(item)
+
+        })
+
+
+    });
+
+}
+
+function publishStory(){
+    let my_form = document.getElementById('editorForm');
+
+    document.addEventListener('click', async (event)=>{
+        let target = event.target;
+        if(!target.closest('.js__editor__publish__btn')) return;
+        let formData = new FormData(my_form);
+        let url = app_url + '/editor/save_story';
+
+        let response = await sendData(formData,url)
 
 
     })
 }
 
+
 document.addEventListener("DOMContentLoaded", function () {
 
     const csrfToken = document.querySelector('[name=csrf-token]').content;
     keyDown()
-    // removeLine()
+    publishStory()
+
 })
--- resources/views/user/editor/editor.blade.php
+++ resources/views/user/editor/editor.blade.php
@@ -5,17 +5,21 @@
 @section('headers')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Sortable/1.10.2/Sortable.min.js"></script>
+{{--<script src="https://cdn.tiny.cloud/1/2l183jmgeohh0y7sb48wid55xnl1fouhc42xs1pb438bztkb/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>--}}
+
 @endsection
 
 @section('content')
 
     <div class="editor__main">
         <div class="container">
+           <div class="editor__wrapper">
 
-           <div class="editor__wrapper js__editor__wrapper">
-               Sapa Shirmadov
+               <div class="editor__btn">
+                   <button class="editor__publish__btn js__editor__publish__btn">Publish</button>
+               </div>
+               <form id="editorForm" class="editorForm js__editor__form" action="" method="POST">
 
-               <form id="editorForm" class="editorForm" action="" method="POST" class="">
                    <div class="editor__line js__editor__line">
                        <div class="editor__plus__main">
                            <svg class="editor__plus" xmlns="http://www.w3.org/2000/svg" width="35" height="24" viewBox="0 0 40 32" fill="none">
@@ -24,30 +28,19 @@
                                <line x1="20.1667" y1="23.752" x2="20.1667" y2="7" stroke="black"/>
                            </svg>
                        </div>
-                       <input id="myTextField" class="editor__text__title js__editor__text" type="text" placeholder="Title blog">
+                       <input id="myTextField" name="text__line__0" class="editor__text__title js__editor__text" data-order="1" type="text" placeholder="Title blog">
                    </div>
-                   <div class="editor__line js__editor__line">
-                       <div class="editor__plus__main">
-                           <svg class="editor__plus" xmlns="http://www.w3.org/2000/svg" width="35" height="24" viewBox="0 0 40 32" fill="none">
-                               <rect x="0.5" y="0.75" width="39" height="30.5" rx="6.5" fill="#FFFBFB" stroke="black"/>
-                               <line x1="10" y1="15.5" x2="30" y2="15.5" stroke="black"/>
-                               <line x1="20.1667" y1="23.752" x2="20.1667" y2="7" stroke="black"/>
-                           </svg>
-                       </div>
-                       <input class="editor__text js__editor__text" type="text" placeholder="Write your article">
-                   </div>
-                   <div class="editor__line js__editor__line">
-                       <div class="editor__plus__main">
-                           <svg class="editor__plus" xmlns="http://www.w3.org/2000/svg" width="35" height="24" viewBox="0 0 40 32" fill="none">
-                               <rect x="0.5" y="0.75" width="39" height="30.5" rx="6.5" fill="#FFFBFB" stroke="black"/>
-                               <line x1="10" y1="15.5" x2="30" y2="15.5" stroke="black"/>
-                               <line x1="20.1667" y1="23.752" x2="20.1667" y2="7" stroke="black"/>
-                           </svg>
-                       </div>
-                       <input class="editor__text js__editor__text" type="text" placeholder="Write your Story">
-                   </div>
+{{--                   <div class="editor__line js__editor__line">--}}
+{{--                       <div class="editor__plus__main">--}}
+{{--                           <svg class="editor__plus" xmlns="http://www.w3.org/2000/svg" width="35" height="24" viewBox="0 0 40 32" fill="none">--}}
+{{--                               <rect x="0.5" y="0.75" width="39" height="30.5" rx="6.5" fill="#FFFBFB" stroke="black"/>--}}
+{{--                               <line x1="10" y1="15.5" x2="30" y2="15.5" stroke="black"/>--}}
+{{--                               <line x1="20.1667" y1="23.752" x2="20.1667" y2="7" stroke="black"/>--}}
+{{--                           </svg>--}}
+{{--                       </div>--}}
+{{--                       <input class="editor__text js__editor__text" name="message2" type="text" data-order="2" placeholder="Write your article">--}}
+{{--                   </div>--}}
                </form>
-
            </div>
 
         </div>
@@ -55,11 +48,9 @@
 @endsection
 
 @section('js')
-
     <script type="module" src="{{asset('js/user/editor.js')}}"></script>
-
 <script>
-    const dragArea = document.querySelector('.js__editor__wrapper');
+    const dragArea = document.querySelector('.editorForm');
     new Sortable(dragArea, {
         animation:350
     });
