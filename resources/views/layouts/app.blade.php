ad07a71c9dbeee37ffdef83b43a12cd57e4eb898
sapa <sapa.sh@wewizards.ru>
2024-05-01 19:48:41 +0300

    last

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,8 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
+{{--        <link rel="stylesheet" href="https://wagt.space/static/css/widget.css">--}}
+{{--        <script src="https://wagt.space/static/js/widget.js"></script>--}}
         <!-- Styles -->
 {{--        <link rel="stylesheet" href="{{ asset('css/app.css') }}">--}}
         @guest
